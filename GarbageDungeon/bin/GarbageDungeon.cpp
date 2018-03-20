e8bfd84592ae45c0e3e9e14ed3c00590c040d8a7
Robert J Dunski <rdunski411@gmail.com>
2018-03-20 14:01:11 -0600

    cleaned up file structure

---
+++ GarbageDungeon/bin/GarbageDungeon.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include "SDL.h"
+#include"Game.h"
+#include"Sprite.h"
+using namespace std;
+
+Game GD;
+
+int main(int argc, char* argv[])
+{
+	GD.init();
+	GD.run();
+	return 0;
+}
